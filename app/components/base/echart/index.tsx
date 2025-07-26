66abf3a6367bca436e610e6ecd3e5eec2282ec3d
sixieyou <1369066793@qq.com>
2025-07-26 17:48:02 +0800

    fix echart details

--- app/components/base/echart/index.tsx
+++ app/components/base/echart/index.tsx
@@ -14,7 +14,29 @@ const EChart: React.FC<EChartProps> = ({ option, style }) => {
             return
 
         const chart = echarts.init(chartRef.current)
-        chart.setOption(option)
+        chart.setOption({
+            ...option,
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow',
+                },
+                formatter: (params: any) => {
+                    const [firstParam] = params
+                    return `
+            <div style="margin: 0 0 4px 0;font-weight:500">
+              name: ${firstParam.name}<br/>
+              value: ${firstParam.value}
+            </div>
+          `
+                },
+                backgroundColor: 'rgba(255,255,255,0.9)',
+                borderColor: '#eee',
+                textStyle: {
+                    color: '#333',
+                },
+            },
+        })
 
         const handleResize = () => chart.resize()
         window.addEventListener('resize', handleResize)
